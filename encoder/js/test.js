502899285bd5b1301ff0d989db2d441e59e03686
Святослав <64555707+orlov11@users.noreply.github.com>
2021-08-03 23:11:19 +0300

    Add files via upload

--- encoder/js/test.js
+++ encoder/js/test.js
@@ -1,5 +1,15 @@
-const str = 'Строка'
+const arr = ['я', 'люблю','Асю','♠','4']
+//             0      1   2   3   4 
 
-str.readAsText(file, 'CP1251')
+// arr.forEach((i,q)=>{
+//  console.log(`Порядковый номер ${q } его значение ${i}`)
 
-console.log(str)
+// })
+
+
+
+function sun(a,b){
+    return a + b
+}
+
+console.log(sun(22,33))
